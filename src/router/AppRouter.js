37701a6f7c231179b4b60dc1b9acafe189a72359
IAN <julian63k@gmail.com>
2024-08-26 09:38:12 -0500

    Essencials

---
+++ src/router/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function AppRouter() {
+    return (
+
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path="/" component={Carrousel} /> */}
+            </Routes>
+        </BrowserRouter>
+
+    )
+}
+
+export default AppRouter
